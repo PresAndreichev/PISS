7dfe3f6190ea895a39746b543210989e3af05b3b
Presian_Andreichev <ganjatavee@gmail.com>
2025-01-12 17:36:51 +0200

    Added my things

--- PISSDjango/App/views/serve_html.py
+++ PISSDjango/App/views/serve_html.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import os
+from django.conf import settings
 
 def serve_html(request, page_name):
     try:
-        with open(f'App/Static/html/{page_name}.html', 'r',encoding='utf-8') as file:
+        file_path = os.path.join(settings.BASE_DIR, 'App/Static/html', f"{page_name}.html")
+        with open(file_path, 'r',encoding='utf-8') as file:
             return HttpResponse(file.read(),content_type="text/html")
     except FileNotFoundError:
         return HttpResponse("Homepage not found",status=404)
