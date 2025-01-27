596b948f50e8e0841e3189c112a497db5749e35e
Presian_Andreichev <ganjatavee@gmail.com>
2025-01-27 13:52:33 +0200

    Changed the path to absolutes in main.html

--- PISSDjango/app/urls.py
+++ PISSDjango/app/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('api/disable-profile/', disable_profile, name='disable_profile'),  # Disable profile route
     path('api/change_password/', change_password, name='change_password'),  # Room schedule route
     path('api/register/', register_user, name='register_user'),
-    path('', lambda request: serve_html(request, 'index'), name='index')  # Homepage route
+    path('', lambda request: serve_html(request, 'main'), name='main')  # Homepage route
 ]
 # Add the URLS, of the get_lesson, take_attendance, get_rooms, reserve_room, get_saved_rooms
